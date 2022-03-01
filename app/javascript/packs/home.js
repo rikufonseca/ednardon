b22b19a084a710691306b391b8f0727445803ef6
Erika Fonseca <fonsecarika@gmail.com>
2022-03-02 00:50:03 +0200

    animation navbar

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,3 +1,11 @@
 export function home () {
- 
+ const text =  document.querySelector('.txt').innerText;
+
+ const moveIt = () => {
+   text.repeat(2);
+   text.classList.add('moveit');
+ }
+
+ window.addEventListener( 'load', moveIt );
+
 }
