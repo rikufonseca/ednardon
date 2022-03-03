8b98bc85e1131050b1d6d3d09693909a932e1487
Erika Fonseca <fonsecarika@gmail.com>
2022-03-04 00:16:49 +0200

    need pictures

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,3 +1,14 @@
 export function home () {
+ const text = document.querySelector('.txt');
+
+ const elementInView = (text, scrollOffset = 100) => {
+  const elementTop = text.getBoundingClientRect().top;
+ 
+  return (
+    elementTop <= 
+    ((window.innerHeight || document.documentElement.clientHeight) - scrollOffset)
+  );
+ };
+
 
 }
