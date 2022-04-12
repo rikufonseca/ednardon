e3e7dc562bf359bda722e989f25e3f7ac078a768
Erika Fonseca <fonsecarika@gmail.com>
2022-04-12 18:24:05 +0300

    js to implant

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,14 +1,3 @@
 export function home () {
- const text = document.querySelector('.txt');
-
- const elementInView = (text, scrollOffset = 100) => {
-  const elementTop = text.getBoundingClientRect().top;
- 
-  return (
-    elementTop <= 
-    ((window.innerHeight || document.documentElement.clientHeight) - scrollOffset)
-  );
- };
-
-
+  phrase = document.querySelector(".txt");
 }
