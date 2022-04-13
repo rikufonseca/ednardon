c068ba50e8903fc2f8cd6ee69672d66829256c78
Erika Fonseca <fonsecarika@gmail.com>
2022-04-13 23:15:46 +0300

    height pos unset

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,3 +1,34 @@
 export function home () {
-  phrase = document.querySelector(".txt");
-}
+  const allDiv = document.querySelectorAll(".img");
+  const startPoint = document.getElementById("description");
+
+   const startPointPosition = startPoint.offsetTop;
+   const startPointHeight = startPoint.offsetHeight;
+
+   let allDivPosition = [];
+   let allDivHeight = [];
+
+   
+   
+   window.addEventListener("scroll", function(){
+    allDiv.forEach( div => allDivPosition.push(div.offsetTop));
+    console.log(allDivPosition);
+    allDiv.forEach( div => allDivHeight.push(div.offsetHeight));
+    // console.log(allDivHeight);
+
+    if ( allDivPosition.some( item => item > startPointPosition + startPointHeight) ) {
+      console.log('Boom !');
+    // const newHeight = allDivHeight + 1;
+    // allDiv.style.height = newHeight + "px";
+    } else {
+      console.log('Baam !')
+    // const newHeight = allDivHeight - 1;
+    // allDiv.style.height = newHeight - "px";
+    }
+
+    allDivPosition = [];
+    allDivHeight = [];
+   })
+
+};
+ 
