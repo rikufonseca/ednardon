efab01d86cd6223a6e7bc99e4d18bcfcc5388f9a
Erika Fonseca <fonsecarika@gmail.com>
2022-04-20 20:14:45 +0300

    still pb but now it change size atleast

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,98 +1,6 @@
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger);
-
 export function home () {
-  let animation = gsap.fromTo(".img-one", {scaleY:1},{scaleY:1.5}, 0);
   
-  ScrollTrigger.create({
-    trigger: ".down",
-    start: "top top += 47px",
-    end: "bottom 47px",
-    markers: {startColor: "black", endColor: "black"},
-    scrub: true,
-    animation: animation,
-  });
-
-  let animation1 = gsap.fromTo(".img-two", {scaleY:1, scaleX:1},{scaleY:1.5, scaleX:1}, 0);
-
-  ScrollTrigger.create({
-    trigger: ".img-one",
-    start: "top top += 47px",
-    end: "bottom 47px",
-    markers: {startColor: "black", endColor: "black"},
-    scrub: true,
-    animation: animation1,
-  });
-
-  let animation2 = gsap.fromTo(".img-three", {scaleY:1, scaleX:1},{scaleY:1.5, scaleX:1}, 0);
-
-  ScrollTrigger.create({
-    trigger: ".img-two",
-    start: "top top += 47px",
-    end: "bottom 47px",
-    markers: {startColor: "black", endColor: "black"},
-    scrub: true,
-    animation: animation2,
-  });
-
-  let animation3 = gsap.fromTo(".img-four", {scaleY:1, scaleX:1},{scaleY:1.5, scaleX:1}, 0);
-
-  ScrollTrigger.create({
-    trigger: ".img-three",
-    start: "top top += 47px",
-    end: "bottom 47px",
-    markers: {startColor: "black", endColor: "black"},
-    scrub: true,
-    animation: animation3,
-  });
-
-  let animation4 = gsap.fromTo(".img-five", {scaleY:1, scaleX:1},{scaleY:1.5, scaleX:1}, 0);
-
-  ScrollTrigger.create({
-    trigger: ".img-four",
-    start: "top top += 47px",
-    end: "bottom 47px",
-    markers: {startColor: "black", endColor: "black"},
-    scrub: true,
-    animation: animation4,
-  });
-
-  let animation5 = gsap.fromTo(".img-six", {scaleY:1, scaleX:1},{scaleY:1.5, scaleX:1}, 0);
-
-  ScrollTrigger.create({
-    trigger: ".img-five",
-    start: "top top += 47px",
-    end: "bottom 47px",
-    markers: {startColor: "black", endColor: "black"},
-    scrub: true,
-    animation: animation5,
-  });
-
-  let animation6 = gsap.fromTo(".img-seven", {scaleY:1, scaleX:1},{scaleY:1.5, scaleX:1}, 0);
-
-  ScrollTrigger.create({
-    trigger: ".img-six",
-    start: "top top += 47px",
-    end: "bottom 47px",
-    markers: {startColor: "black", endColor: "black"},
-    scrub: true,
-    animation: animation6,
-  });
-
-  let animation7 = gsap.fromTo(".img-eight", {scaleY:1, scaleX:1},{scaleY:1.5, scaleX:1}, 0);
-
-  ScrollTrigger.create({
-    trigger: ".img-seven",
-    start: "top top += 47px",
-    end: "bottom 47px",
-    markers: {startColor: "black", endColor: "black"},
-    scrub: true,
-    animation: animation7,
-  });
-
-    let one = document.querySelector(".be");
+    let one = document.querySelector(".test");
   //  const two = document.querySelector(".img-two");
   //  const three = document.querySelector(".img-three");
   //  const four = document.querySelector(".img-four");
@@ -100,29 +8,29 @@ export function home () {
   //  const six = document.querySelector(".img-six");
   //  const seven = document.querySelector(".img-seven");
   //  const eight = document.querySelector(".mail");
-  let fontProp = one.style.fontVariationSettings = '"wght" 76.9721, "wdth" 437.395246, "opsz" 32.4196, "XOPQ" 106.063, "XTRA" 371.5, "YOPQ" 44.4875, "YTLC" 600, "YTSE" 13, "GRAD" 88, "XTCH" 926.111, "YTCH" 1068.3, "YTAS" 808.611, "YTDE" 238.611, "YTUC" 924.583, "YTRA" 908.333';
-  console.log(fontProp);
-    
   window.addEventListener("scroll", function(){
-    const onePosition = one.getBoundingClientRect();
-    const y = onePosition.top;
+    const maxHeight = 1200;
+    const minHeight = 0;
 
-    
-//  const getByKey = (key) => {
-//    let keyValues = new Map();
-//    let tempArr = fontProp.split(',');
-//    console.log(tempArr);
-//    tempArr.forEach(x => {
-//      let keyVal = x.trim().split(' ');
-//      keyValues.set(keyVal[0], keyVal[1]);
-//    });
-//    return keyValues.get(key);
-//  }
-//  let value = getByKey('"YTRA"');
+    const posTop = 0;
+    const posBottom = 25;
 
-  if ( y <= 127.79545593261719 && y >= 25.25 ){ 
-       
-    };
+    let scrollPosition =
+    (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+
+    const percent = scrollPosition / 0.99;
+    const heightScale = percent * maxHeight - minHeight;
+    const posScale = percent * posBottom + posTop;
+
+    const newHeightValue = scrollPosition > 0.99 ? maxHeight : heightScale;
+
+    if (scrollPosition >= 0.99) {
+      one.style.setProperty("--ytra", minHeight);
+      one.style.setProperty("--pos", posBottom + '%');
+    } else {
+      one.style.setProperty("--ytra", newHeightValue);
+      one.style.setProperty("--pos", posScale + '%');
+    }
 
   });
 }
