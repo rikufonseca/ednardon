e92fae8d0ae9402243af56e5cd4a230b40c01d40
Loris Reynaud <loris.reynaud@gmail.com>
2022-04-23 01:33:30 +0200

    css to start more little

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -14,90 +14,70 @@ export function home () {
   const nine = lines[8];
   const ten = lines[9];
 
-  let oneMinHeight = 900;
-  let twoMinHeight = 900;
-  let threeMinHeight = 900;
-  let fourMinHeight = 900;
-  let fiveMinHeight = 900;
-  let sixMinHeight = 900;
-  let sevenMinHeight = 900;
-  let eightMinHeight = 900;
-  let nineMinHeight = 900;
-  let tenMinHeight = 900;
+  let oneMinHeight = 700;
+  let twoMinHeight = 700;
+  let threeMinHeight = 700;
+  let fourMinHeight = 700;
+  let fiveMinHeight = 700;
+  let sixMinHeight = 700;
+  let sevenMinHeight = 700;
+  let eightMinHeight = 700;
+  let nineMinHeight = 700;
+  let tenMinHeight = 700;
   
   window.addEventListener("scroll", function(){
-    // const maxHeight = 1200;
-    const posTop = 0;
-    const posBottom = 25;
-    const scrollPositionLimit = 0.23;
-
     let scrollPosition =
     (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
     console.log(scrollPosition);
 
-    const percent = scrollPosition / 0.99;
-    // const heightScale = percent * maxHeight - MinHeight;
-    const posScale = percent * posBottom + posTop;
-    // console.log(posScale);
-
-    // const newHeightValue = scrollPosition > scrollPositionLimit ? maxHeight : heightScale;
-
-    if (scrollPosition >= 0.63) {
-      tenMinHeight = 900;
+    if (scrollPosition >= 0.60) {
       ten.classList.add('line-grow')
-      ten.style.setProperty("--ytra", tenMinHeight +=4);
+      ten.style.setProperty("--ytra", tenMinHeight +=5);
     }
     
-    if (scrollPosition >= 0.50) {
+    if (scrollPosition >= 0.56) {
       nine.classList.add('line-grow')
-      nine.style.setProperty("--ytra", nineMinHeight +=4);
+      nine.style.setProperty("--ytra", nineMinHeight +=5);
     }
 
-    if (scrollPosition >= 0.53) {
+    if (scrollPosition >= 0.52) {
       eight.classList.add('line-grow')
-      eight.style.setProperty("--ytra", eightMinHeight +=4);
+      eight.style.setProperty("--ytra", eightMinHeight +=5);
     } 
     
-    if (scrollPosition >= 0.50) {
+    if (scrollPosition >= 0.46) {
       seven.classList.add('line-grow')
-      seven.style.setProperty("--ytra", sevenMinHeight +=4);
+      seven.style.setProperty("--ytra", sevenMinHeight +=5);
     } 
 
-    if (scrollPosition >= 0.45) {
+    if (scrollPosition >= 0.42) {
       six.classList.add('line-grow')
-      six.style.setProperty("--ytra", sixMinHeight +=4);
+      six.style.setProperty("--ytra", sixMinHeight +=5);
     } 
     
-    if (scrollPosition >= 0.41) {
+    if (scrollPosition >= 0.38) {
       five.classList.add('line-grow')
-      five.style.setProperty("--ytra", fiveMinHeight +=4);
+      five.style.setProperty("--ytra", fiveMinHeight +=5);
     } 
     
-    if (scrollPosition >= 0.36) {
+    if (scrollPosition >= 0.34) {
       four.classList.add('line-grow')
-      four.style.setProperty("--ytra", fourMinHeight +=4);
+      four.style.setProperty("--ytra", fourMinHeight +=5);
     } 
     
-    if (scrollPosition >= 0.31) {
+    if (scrollPosition >= 0.30) {
       three.classList.add('line-grow')
-      three.style.setProperty("--ytra", threeMinHeight +=4);
+      three.style.setProperty("--ytra", threeMinHeight +=5);
     } 
     
-    if (scrollPosition >= 0.27) {
+    if (scrollPosition >= 0.26) {
       two.classList.add('line-grow')
-      two.style.setProperty("--ytra", twoMinHeight +=4);
+      two.style.setProperty("--ytra", twoMinHeight +=5);
     } 
     
     if (scrollPosition >= 0.220) {
       one.classList.add('line-grow')
-      one.style.setProperty("--ytra", oneMinHeight +=4);
+      one.style.setProperty("--ytra", oneMinHeight +=5);
     }
-
-    // switch (scrollPosition) {
-    //   case scrollPosition >= 0.23 :
-    //     console.log("gogogo");
-    //     one.classList.add('line-grow');
-    //   case scrollPosition <= 0.23 :
-    // }
   });
 }
