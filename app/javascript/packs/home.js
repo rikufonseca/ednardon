654169baf4b866d70a84b2cf07269859b228009a
Loris Reynaud <loris.reynaud@gmail.com>
2022-04-22 23:20:23 +0200

    lines grow when the first is activated

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,36 +1,53 @@
 export function home () {
   
-    let one = document.querySelector(".test");
-  //  const two = document.querySelector(".img-two");
-  //  const three = document.querySelector(".img-three");
-  //  const four = document.querySelector(".img-four");
-  //  const five = document.querySelector(".img-five");
-  //  const six = document.querySelector(".img-six");
-  //  const seven = document.querySelector(".img-seven");
-  //  const eight = document.querySelector(".mail");
-  window.addEventListener("scroll", function(){
-    const maxHeight = 1200;
-    const minHeight = 0;
+  const lines = document.querySelectorAll(".home-line");
 
+  // const one = lines[0];
+  // const two = lines[1];
+  // const three = lines[2];
+  // const four = lines[3];
+  // const five = lines[4];
+  // const six = lines[5];
+  // const seven = lines[6];
+  // const eight = lines[7];
+  // const nine = lines[8];
+  // const ten = lines[9];
+
+  let minHeight = 900;
+
+  
+  window.addEventListener("scroll", function(){
+    // const maxHeight = 1200;
+    
     const posTop = 0;
     const posBottom = 25;
+    const scrollPositionLimit = 0.23;
 
     let scrollPosition =
     (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+    // console.log(scrollPosition);
 
     const percent = scrollPosition / 0.99;
-    const heightScale = percent * maxHeight - minHeight;
+    // const heightScale = percent * maxHeight - minHeight;
     const posScale = percent * posBottom + posTop;
+    // console.log(posScale);
 
-    const newHeightValue = scrollPosition > 0.99 ? maxHeight : heightScale;
-
-    if (scrollPosition >= 0.99) {
-      one.style.setProperty("--ytra", minHeight);
-      one.style.setProperty("--pos", posBottom + '%');
-    } else {
-      one.style.setProperty("--ytra", newHeightValue);
-      one.style.setProperty("--pos", posScale + '%');
-    }
-
+    // const newHeightValue = scrollPosition > scrollPositionLimit ? maxHeight : heightScale;
+    const activeLines = lines.filter(line => {
+      if (scrollPosition <= scrollPositionLimit) {
+        // one[0].style.setProperty("position", "fixed");
+        minHeight = 900;
+        line.style.setProperty("--ytra", minHeight);
+        // console.log(minHeight);
+        // one[0].style.removeProperty("--pos", posBottom + "%");
+      } else {
+        // one[0].style.setProperty("position", "fixed");
+        line.style.setProperty("--ytra", minHeight += 0.7);
+        // console.log(minHeight);
+        // one[0].style.setProperty("--pos", posScale + '%');
+      }  
+    });
+    console.log(activeLines);
   });
+  
 }
