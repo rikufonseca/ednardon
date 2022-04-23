f992fcfc8b71772fbb1d124390928a2dd6911bb9
Loris Reynaud <loris.reynaud@gmail.com>
2022-04-23 19:50:12 +0200

    changed scroll with wheel, it works better

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,5 +1,4 @@
-export function home () {
-  
+export function home () {  
   let lines = document.querySelectorAll(".home-line");
   // lines = Array.prototype.slice.call(lines);
 
@@ -14,70 +13,76 @@ export function home () {
   const nine = lines[8];
   const ten = lines[9];
 
-  let oneMinHeight = 700;
-  let twoMinHeight = 700;
-  let threeMinHeight = 700;
-  let fourMinHeight = 700;
-  let fiveMinHeight = 700;
-  let sixMinHeight = 700;
-  let sevenMinHeight = 700;
-  let eightMinHeight = 700;
-  let nineMinHeight = 700;
-  let tenMinHeight = 700;
+  let oneHeight = 300;
+  let twoHeightVar = 400;
+  let threeHeightVar = 400;
+  let fourHeightVar = 400;
+  let fiveHeightVar = 400;
+  let sixHeightVar = 400;
+  let sevenHeightVar = 400;
+  let eightHeightVar = 400;
+  let nineHeightVar = 400;
+  let tenHeightVar = 400;
+
   
-  window.addEventListener("scroll", function(){
+  
+  window.addEventListener("wheel", function(event){
+    
+
     let scrollPosition =
     (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-    console.log(scrollPosition);
+    // console.log(scrollPosition);
 
-    if (scrollPosition >= 0.56) {
-      ten.classList.add('line-grow')
-      ten.style.setProperty("--ytra", tenMinHeight +=7);
-    }
-    
-    if (scrollPosition >= 0.52) {
-      nine.classList.add('line-grow')
-      nine.style.setProperty("--ytra", nineMinHeight +=7);
-    }
+    console.log(event.deltaY);
 
-    if (scrollPosition >= 0.48) {
-      eight.classList.add('line-grow')
-      eight.style.setProperty("--ytra", eightMinHeight +=7);
-    } 
+    // if (scrollPosition >= 0.56) {
+    //   scrollDirection(ten);
+    // }
     
-    if (scrollPosition >= 0.44) {
-      seven.classList.add('line-grow')
-      seven.style.setProperty("--ytra", sevenMinHeight +=7);
-    } 
+    // if (scrollPosition >= 0.52) {
+    //   scrollDirection(nine);
+    // }
 
-    if (scrollPosition >= 0.38) {
-      six.classList.add('line-grow')
-      six.style.setProperty("--ytra", sixMinHeight +=7);
-    } 
+    // if (scrollPosition >= 0.48) {
+    //   scrollDirection(eight);
+    // } 
     
-    if (scrollPosition >= 0.34) {
-      five.classList.add('line-grow')
-      five.style.setProperty("--ytra", fiveMinHeight +=7);
-    } 
+    // if (scrollPosition >= 0.44) {
+    //   scrollDirection(seven);
+    // } 
+
+    // if (scrollPosition >= 0.38) {
+    //   scrollDirection(six);
+    // } 
     
-    if (scrollPosition >= 0.30) {
-      four.classList.add('line-grow')
-      four.style.setProperty("--ytra", fourMinHeight +=7);
-    } 
+    // if (scrollPosition >= 0.34) {
+    //   scrollDirection(five);
+    // } 
     
-    if (scrollPosition >= 0.26) {
-      three.classList.add('line-grow')
-      three.style.setProperty("--ytra", threeMinHeight +=7);
-    } 
+    // if (scrollPosition >= 0.30) {
+    //   scrollDirection(four);
+    // } 
     
-    if (scrollPosition >= 0.22) {
-      two.classList.add('line-grow')
-      two.style.setProperty("--ytra", twoMinHeight +=7);
-    } 
+    // if (scrollPosition >= 0.26) {
+    //   scrollDirection(three);
+    // } 
     
-    if (scrollPosition >= 0.18) {
-      one.classList.add('line-grow')
-      one.style.setProperty("--ytra", oneMinHeight +=7);
-    }
+    // if (scrollPosition >= 0.25) {
+    //   scrollDirection(two);
+    // } 
+    
+    
+      if (event.deltaY < 0 && scrollPosition < 0.28) {
+        one.classList.remove('static-line');
+        one.classList.add('line-grow');
+        one.style.setProperty("--ytra", oneHeight > 300 ? oneHeight -= 10 : oneHeight = 300);
+      } else if (event.deltaY > 0 && scrollPosition > 0.22) {
+        one.classList.remove('static-line');
+        one.classList.add('line-grow');
+        one.style.setProperty("--ytra", oneHeight < 1200 ? oneHeight += 10 : oneHeight = 1200);
+      }
+      
+      
+    
   });
 }
