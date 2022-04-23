e4378c9d180dd442386273ec3010d0684ae15e2c
Loris Reynaud <loris.reynaud@gmail.com>
2022-04-23 14:30:50 +0200

    deleted accents, not responsive

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -30,54 +30,54 @@ export function home () {
     (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
     console.log(scrollPosition);
 
-    if (scrollPosition >= 0.60) {
-      ten.classList.add('line-grow')
-      ten.style.setProperty("--ytra", tenMinHeight +=5);
-    }
-    
     if (scrollPosition >= 0.56) {
+      ten.classList.add('line-grow')
+      ten.style.setProperty("--ytra", tenMinHeight +=7);
+    }
+    
+    if (scrollPosition >= 0.52) {
       nine.classList.add('line-grow')
-      nine.style.setProperty("--ytra", nineMinHeight +=5);
+      nine.style.setProperty("--ytra", nineMinHeight +=7);
     }
 
-    if (scrollPosition >= 0.52) {
+    if (scrollPosition >= 0.48) {
       eight.classList.add('line-grow')
-      eight.style.setProperty("--ytra", eightMinHeight +=5);
+      eight.style.setProperty("--ytra", eightMinHeight +=7);
     } 
     
-    if (scrollPosition >= 0.46) {
+    if (scrollPosition >= 0.44) {
       seven.classList.add('line-grow')
-      seven.style.setProperty("--ytra", sevenMinHeight +=5);
+      seven.style.setProperty("--ytra", sevenMinHeight +=7);
     } 
 
-    if (scrollPosition >= 0.42) {
-      six.classList.add('line-grow')
-      six.style.setProperty("--ytra", sixMinHeight +=5);
-    } 
-    
     if (scrollPosition >= 0.38) {
-      five.classList.add('line-grow')
-      five.style.setProperty("--ytra", fiveMinHeight +=5);
+      six.classList.add('line-grow')
+      six.style.setProperty("--ytra", sixMinHeight +=7);
     } 
     
     if (scrollPosition >= 0.34) {
-      four.classList.add('line-grow')
-      four.style.setProperty("--ytra", fourMinHeight +=5);
+      five.classList.add('line-grow')
+      five.style.setProperty("--ytra", fiveMinHeight +=7);
     } 
     
     if (scrollPosition >= 0.30) {
-      three.classList.add('line-grow')
-      three.style.setProperty("--ytra", threeMinHeight +=5);
+      four.classList.add('line-grow')
+      four.style.setProperty("--ytra", fourMinHeight +=7);
     } 
     
     if (scrollPosition >= 0.26) {
-      two.classList.add('line-grow')
-      two.style.setProperty("--ytra", twoMinHeight +=5);
+      three.classList.add('line-grow')
+      three.style.setProperty("--ytra", threeMinHeight +=7);
     } 
     
-    if (scrollPosition >= 0.220) {
+    if (scrollPosition >= 0.22) {
+      two.classList.add('line-grow')
+      two.style.setProperty("--ytra", twoMinHeight +=7);
+    } 
+    
+    if (scrollPosition >= 0.18) {
       one.classList.add('line-grow')
-      one.style.setProperty("--ytra", oneMinHeight +=5);
+      one.style.setProperty("--ytra", oneMinHeight +=7);
     }
   });
 }
