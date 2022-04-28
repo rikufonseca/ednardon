99253eb0b4cc9d33b1197515c02c17f05fddd14f
Erika Fonseca <fonsecarika@gmail.com>
2022-04-28 15:36:24 +0300

    need to stretch the rect

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -4,8 +4,8 @@ import { Draggable } from "gsap/Draggable.js";
 gsap.registerPlugin(Draggable);
 
 export function home () {
-    gsap.fromTo(".helloTop", {y: 17, duration: 0}, {y:-100, duration: 4});
-
+    gsap.fromTo(".helloTop", {y: 17, duration: 0 }, {y:-100, duration: 2, ease:"linear"});
+    gsap.to(".helloBottom", {y:-117, delay: 3, duration: 2, ease:"bounce"});
 
     const stretchBoxInitHeight = 7;
     const maxScrollY = 260;
