80079e7cc7fde308b5c1cef524b12408cdf095b3
Erika Fonseca <fonsecarika@gmail.com>
2022-04-28 15:25:25 +0300

    gsap

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -3,7 +3,13 @@ import { Draggable } from "gsap/Draggable.js";
 
 gsap.registerPlugin(Draggable);
 
-export function home () {  
-  gsap.to(".helloTop", {y: 17});
+export function home () {
+    gsap.fromTo(".helloTop", {y: 17, duration: 0}, {y:-100, duration: 4});
+
+
+    const stretchBoxInitHeight = 7;
+    const maxScrollY = 260;
+    const scrollPosY = 0;
+
 }
 
