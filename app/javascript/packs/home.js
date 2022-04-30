3b0e82181fd4a93ba3370796b0c4d7c391efb70b
Erika Fonseca <fonsecarika@gmail.com>
2022-04-30 14:45:13 +0300

    get the line back up

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -7,6 +7,7 @@ export function home () {
     let animation = gsap.timeline();
     animation
       .fromTo(".helloTop", {y: 17, duration: 0 }, {y:-100, duration: 2, ease:"linear"})
+      .from(".oneOpen", {attr:{y2:169}, duration: 2, clearProps: "y" }, "<" )
       .to(".helloBottom", {y:-117, duration: 2, ease:"bounce"}, "+=0.5");
 
     const stretchBoxInitHeight = 7;
