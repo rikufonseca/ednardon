6a9b63637c3c8c24588908af79c8f1dbb8ffa980
Erika Fonseca <fonsecarika@gmail.com>
2022-05-03 13:12:47 +0300

    ok just need it on scroll

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -7,9 +7,9 @@ export function home () {
     let animation = gsap.timeline();
     animation
       .fromTo(".helloTop", {y: 20, duration: 0 },{y:-100, duration: 1, ease:"linear"})
-      .fromTo(".oneOpen", {attr:{y2:169}, duration:1}, {attr: {y2: 54}, duration:1}, "<")
-      .fromTo(".oneOpen", {attr:{y2:54}, duration: 1.5}, {attr:{y1: 54}, duration: 1.5})
-      .to(".helloBottom", {y:-120.2, duration: 1}, "<");
+      .fromTo(".open", {attr:{y2:169}, duration:1, ease: "linear" }, {attr: {y2: 54}, duration:1, ease: "linear" }, "<")
+      .fromTo(".open", {attr:{y2:54}, duration: 1, ease: "linear"}, {attr:{y1: 54}, duration: 1, ease: "linear" })
+      .to(".helloBottom", {y:-120.2, duration: 1, ease: "linear"}, "<");
 
     const stretchBoxInitHeight = 7;
     const maxScrollY = 260;
