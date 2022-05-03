2e394739dc31f7c29e3a7948ca70a0822500d227
Erika Fonseca <fonsecarika@gmail.com>
2022-05-03 22:40:56 +0300

    ok only put markers well

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,19 +1,30 @@
 import { gsap } from "gsap";
-import { Draggable } from "gsap/Draggable.js";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(Draggable);
+gsap.registerPlugin(ScrollTrigger);
 
 export function home () {
     let animation = gsap.timeline();
     animation
-      .fromTo(".helloTop", {y: 20, duration: 0 },{y:-100, duration: 1, ease:"linear"})
-      .fromTo(".open", {attr:{y2:169}, duration:1, ease: "linear" }, {attr: {y2: 54}, duration:1, ease: "linear" }, "<")
-      .fromTo(".open", {attr:{y2:54}, duration: 1, ease: "linear"}, {attr:{y1: 54}, duration: 1, ease: "linear" })
+      .fromTo(".helloTop", {y: 20, duration: 0 }, 
+        { y:-100, duration: 1, ease:"linear"})
+
+      .fromTo(".open", {  attr:{y2:169}, duration:1, ease: "linear" }, 
+        { attr: {y2: 54}, duration:1, ease: "linear" }, "<")
+
+      .fromTo(".open", {attr:{y2:54}, duration: 1, ease: "linear"}, 
+        {attr:{y1: 54}, duration: 1, ease: "linear" })
+    
       .to(".helloBottom", {y:-120.2, duration: 1, ease: "linear"}, "<");
 
-    const stretchBoxInitHeight = 7;
-    const maxScrollY = 260;
-    const scrollPosY = 0;
+      ScrollTrigger.create({
+        trigger: ".wholeHello",
+        start: "top 200px",
+        end: "center",
+        markers: {startColor: "black", endColor: "black"},
+        scrub: true,
+        animation: animation
+      });
 
 }
 
