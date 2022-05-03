c2791854c1448a23ec1d391343416ede1e5ab85b
Erika Fonseca <fonsecarika@gmail.com>
2022-05-03 12:24:19 +0300

    animation ok need to do on all point

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -6,9 +6,10 @@ gsap.registerPlugin(Draggable);
 export function home () {
     let animation = gsap.timeline();
     animation
-      .fromTo(".helloTop", {y: 17, duration: 0 }, {y:-100, duration: 2, ease:"linear"})
-      .from(".oneOpen", {attr:{y2:169}, duration: 2, clearProps: "y" }, "<" )
-      .to(".helloBottom", {y:-117, duration: 2, ease:"bounce"}, "+=0.5");
+      .fromTo(".helloTop", {y: 20, duration: 0 },{y:-100, duration: 1, ease:"linear"})
+      .fromTo(".oneOpen", {attr:{y2:169}, duration:1}, {attr: {y2: 54}, duration:1}, "<")
+      .fromTo(".oneOpen", {attr:{y2:54}, duration: 2.3}, {attr:{y1: 54}, duration: 2.3})
+      .to(".helloBottom", {y:-120.2, duration: 2}, "<");
 
     const stretchBoxInitHeight = 7;
     const maxScrollY = 260;
