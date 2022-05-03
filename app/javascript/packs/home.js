8e40cbca74a81004e7647780c29e1f7c4ec6f11c
Erika Fonseca <fonsecarika@gmail.com>
2022-05-03 12:27:17 +0300

    did a bit quicker

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -8,8 +8,8 @@ export function home () {
     animation
       .fromTo(".helloTop", {y: 20, duration: 0 },{y:-100, duration: 1, ease:"linear"})
       .fromTo(".oneOpen", {attr:{y2:169}, duration:1}, {attr: {y2: 54}, duration:1}, "<")
-      .fromTo(".oneOpen", {attr:{y2:54}, duration: 2.3}, {attr:{y1: 54}, duration: 2.3})
-      .to(".helloBottom", {y:-120.2, duration: 2}, "<");
+      .fromTo(".oneOpen", {attr:{y2:54}, duration: 1.5}, {attr:{y1: 54}, duration: 1.5})
+      .to(".helloBottom", {y:-120.2, duration: 1}, "<");
 
     const stretchBoxInitHeight = 7;
     const maxScrollY = 260;
