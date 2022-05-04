9fddfd10933a51d73791d05af7701221ed2a4e1b
Erika Fonseca <fonsecarika@gmail.com>
2022-05-04 10:51:38 +0300

    all ok need approvement to continue

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -7,20 +7,20 @@ export function home () {
     let animation = gsap.timeline();
     animation
       .fromTo(".helloTop", {y: 20, duration: 0 }, 
-        { y:-100, duration: 1, ease:"linear"})
+        { y:-100, duration: 0.3, ease:"linear"})
 
-      .fromTo(".open", {  attr:{y2:169}, duration:1, ease: "linear" }, 
-        { attr: {y2: 54}, duration:1, ease: "linear" }, "<")
+      .fromTo(".open", {  attr:{y2:169}, duration:0.3, ease: "linear" }, 
+        { attr: {y2: 54}, duration:0.3, ease: "linear" }, "<")
 
-      .fromTo(".open", {attr:{y2:54}, duration: 1, ease: "linear"}, 
-        {attr:{y1: 54}, duration: 1, ease: "linear" })
+      .fromTo(".open", {attr:{y2:54}, duration: 0, ease: "linear"}, 
+        {attr:{y1: 54}, duration: 0.1, ease: "linear" })
     
-      .to(".helloBottom", {y:-120.2, duration: 1, ease: "linear"}, "<");
+      .to(".helloBottom", {y:-120.2, duration: 0.1, ease: "linear"}, "<");
 
       ScrollTrigger.create({
         trigger: ".wholeHello",
-        start: "top 200px",
-        end: "center",
+        start: "top 320px",
+        end: "top 128px",
         markers: {startColor: "black", endColor: "black"},
         scrub: true,
         animation: animation
