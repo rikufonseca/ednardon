5be34a82473d2f493b76badf3a30ad746c64f1d3
erika fonseca <fonsecarika@gmail.com>
2022-05-29 22:54:59 +0300

    ok

--- app/javascript/packs/home_iam.js
+++ app/javascript/packs/home_iam.js
@@ -6,5 +6,23 @@ gsap.registerPlugin(ScrollTrigger);
 export function home_iam() {
   let animation = gsap.timeline();
   animation
-  console.log("ok");
+    .fromTo(".iAmTop", { y: 10, duration: 0 },
+      { y: -157, duration: 0.30, ease: "linear" })
+
+    .fromTo(".openFour", { attr: { y2: 466 }, duration: 0.30, ease: "linear" }, { attr: { y1: 299 }, duration: 0.30, ease: "linear" }, "<")
+
+    .fromTo(".openFour", { attr: { y2: 466 }, duration: 0, ease: "linear" }, { attr: { y2: 299 }, duration: 0.12, ease: "linear" })
+
+    .fromTo(".iAmBottom", { y: 0, duration: 0 }, { y: -167, duration: 0.12, ease: "linear" }, "<");
+
+  ScrollTrigger.create({
+    trigger: ".wholeIAm",
+    start: "top 370px",
+    end: "top 144px",
+    markers: { startColor: "black", endColor: "black" },
+    scrub: true,
+    animation: animation
+  });
+
+
 }
