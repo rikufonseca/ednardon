a77c97974c2ebe1556b928bdf1ed1b49224c692b
erika fonseca <fonsecarika@gmail.com>
2022-05-30 12:06:52 +0300

    based ok

--- app/javascript/packs/home_based.js
+++ app/javascript/packs/home_based.js
@@ -6,5 +6,21 @@ gsap.registerPlugin(ScrollTrigger);
 export function home_based() {
   let animation = gsap.timeline();
   animation
- //   .fromTo(".basedTop", { y: 10, duration: 0 }, { y: -157, duration: 1.30, ease: "linear" })
-  }
+    .fromTo(".basedTop", { y: 10, duration: 0 }, { y: -157, duration: 0.30, ease: "linear" })
+
+    .fromTo(".openFive", { attr: { y2: 588 }, duration: 0.30, ease: "linear" }, { attr: { y1: 421 }, duration: 0.30, ease: "linear" }, "<")
+
+    .fromTo(".openFive", { attr: { y2: 588 }, duration: 0, ease: "linear" }, { attr: { y2: 421 }, duration: 0.12, ease: "linear" })
+
+    .fromTo(".basedBottom", { y: 0, duration: 0 }, { y: -167, duration: 0.12, ease: "linear" }, "<");
+
+  ScrollTrigger.create({
+    trigger: ".wholeBased",
+    start: "top 387px",
+    end: "top 161px",
+    markers: { startColor: "black", endColor: "black" },
+    scrub: true,
+    animation: animation
+  })
+
+}
