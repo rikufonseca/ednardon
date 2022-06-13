3b47e4a43009adf88e8dd7e47df67806b7df5333
erika fonseca <fonsecarika@gmail.com>
2022-06-13 12:45:14 +0300

    ok got responsive but need to make the anim in % to work well

--- app/javascript/packs/home_that.js
+++ app/javascript/packs/home_that.js
@@ -1,17 +1,27 @@
 export function home_that() {
 
-  // const all = document.querySelector("body")
+  const all = document.querySelector("body");
+  const vgOne = document.querySelector("#layer_1");
 
-  // window.addEventListener("wheel", function (event) {
-  //   let scrollPosition =
-  //     (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+  window.addEventListener("wheel", function (event) {
+    let scrollPosition =
+      (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
 
-  //   if(event.deltaY > 0){
-  //     if (scrollPosition > 0.310) {
-  //       all.classList.add("col");
-  //     }
-  //   }
+    console.log(scrollPosition);
 
+      if (event.deltaY > 0) {
+        if (scrollPosition >= 0.3905) {
+          vgOne.classList.add("takeoff");
+        }
+      };
+
+//       if(event.deltaY > 0){
+//        if (scrollPosition > 0.310) {
+//          all.classList.add("col");
+//        }
+//      };
+  });
+//
   //   let screenHeight = document.documentElement.clientHeight;
 
   //   console.log(screenHeight);
